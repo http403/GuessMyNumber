97e695303abf7561647355ec7eccdb2b4288a97f
Hartman Tam <hartmantam@pm.me>
2021-04-24 20:24:19 -0700

    separated the interface and game logic (sort of)
    - struggled to figure out where should the guess loop should belong to

--- GuessMyNumber.py
+++ GuessMyNumber.py
@@ -18,41 +18,82 @@ def prompt(p, data_type='int') -> Union[str, int]:
 class Game:
     def __init__(self):
         self._target: int = 0
+        self._attempt: int = 1
 
         self._lower_bound: int = 1
         self._upper_bound: int = 10
         self._greet_string: str = 'Welcome to the Guess My Number'
         self._quit_string: str = 'Thanks for playing this game!'
 
+        self.roll_target()
+
     @property
-    def greetings(self) -> str:
+    def attempt(self) -> int:
+        return self._attempt
+
+    @property
+    def greet_string(self) -> str:
         return self._greet_string
 
-    def start_game(self) -> None:
-        print(self.greetings)
+    @property
+    def quit_string(self) -> str:
+        return self._quit_string
 
-        while True:
-            print(f"Guess a number between {self._lower_bound} and {self._upper_bound}")
-            print()
+    @property
+    def lower_bound(self) -> int:
+        return self._lower_bound
 
-            self._target = random.randint(self._lower_bound, self._upper_bound)
-            attempt: int = 1
-            while True:
-                n: int = prompt(f"#{attempt} Try")
-                if n < self._target:
-                    print("Too low, try again!")
-                    attempt += 1
-                elif n > self._target:
-                    print("Too high, try again!")
-                    attempt += 1
-                elif n == self._target:
-                    print(f"Congratulation! You got my number in {attempt} {'try' if attempt == 1 else 'tries'}.")
-                    break
-            again = prompt('Play again', 'str')
-            if again.lower() in ['.', 'n', 'no']:
-                break
+    @property
+    def upper_bound(self) -> int:
+        return self._upper_bound
+
+    def again(self) -> None:
+        self.roll_target()
+        self._attempt_reset()
+
+    def roll_target(self) -> None:
+        self._target = self._roll()
+
+    def guess(self, n) -> int:
+        if n < self._target:
+            self._attempt += 1
+            return -1
+        if n > self._target:
+            self._attempt += 1
+            return 1
+        if n == self._target:
+            return 0
+
+    def _roll(self) -> int:
+        return random.randint(self._lower_bound, self._upper_bound)
+
+    def _attempt_reset(self) -> None:
+        self._attempt = 1
 
 
 if __name__ == '__main__':
+    # Display and user interactions happens here
     game = Game()
-    game.start_game()
+
+    print(game.greet_string)
+    while True:
+        print(f"Guess a number between {game.lower_bound} and {game.upper_bound}")
+        print()
+
+        while True:
+            attempt: int = game.attempt
+            guess: int = prompt(f"#{attempt} Try")
+            result = game.guess(guess)
+            if result < 0:
+                print("Too low, try again!")
+            if result > 0:
+                print("Too high, try again!")
+            if result == 0:
+                print(f"Congratulation! You got my number in {attempt} {'try' if attempt == 1 else 'tries'}.")
+                break
+        again: str = prompt("Play again? (./n/no to stop)", 'str')
+        if again.lower() in ['.', 'n', 'no']:
+            break
+        else: game.again()
+
+    print(game.quit_string)
